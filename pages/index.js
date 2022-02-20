16a4e8c8c588861bc5ed3da1efe40ebc4e6b2b21
migramirez2 <tusharvakhare9898@gmail.com>
2022-02-20 17:58:57 -0500

    added smart contract address to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,21 @@
+import abi from '../utils/TokenGenerator.json';
+
 /* import Head from 'next/head'
-import Image from 'next/image' */
+import Image from 'next/image' 
 import styles from '../styles/Home.module.css'
-import { Typography, Paper, Card } from "@mui/material";
+import { Typography, Paper, Card } from "@mui/material"; */
 
 const Home = () => {
+  //Smart Contract Address for TokenGenerator
+  const contractAddress = "0xBB9C6603433A117f0c50B9e54Db5E517bFC1B21b";
+
+  //initializes new abi per ethers.js docs recommendation
+  const contractABI = abi.abi;
+
   return (
-    <Paper>
-      <Typography>Test</Typography>
-    </Paper>
+    <div>
+      <h1>If You Can Read This, Celebrate</h1>
+    </div>
   )
 };
 
