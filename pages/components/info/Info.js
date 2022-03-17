84940c4a53fae54eaa8cd87dc39f744e3462afe6
migramirez2 <tusharvakhare9898@gmail.com>
2022-03-16 20:18:46 -0400

    deployment corrections - vercel

--- pages/components/info/Info.js
+++ pages/components/info/Info.js
@@ -11,7 +11,7 @@ const Info = () => {
                     <h3>
                         First, setup the <a href="https://metamask.io/download/" className='text-link' target="_blank" rel="noreferrer">MetaMask</a> extension in your browser and ensure your wallet is connected to the <a href="https://umbria.network/connect/ethereum-testnet-rinkeby" className='text-link' target="_blank" rel="noreferrer">Rinkeby Test Network</a>. 
                         Then, to power the minting process, you will need to request Rinkeby Testnet Ethereum <a href="https://faucets.chain.link/rinkeby" className='text-link' target="_blank" rel="noreferrer">here</a> (don&apos;t worry - it&apos;s free).
-                        The deposit normally takes a couple of minutes. Once complete, you're ready to mint!
+                        The deposit normally takes a couple of minutes. Once complete, you&apos;re ready to mint!
                     </h3>
                     <div className="two-button">
                             <Link className='button' activeClass="active" to="generate" spy={true} smooth={true} duration={500} >
